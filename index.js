c3afbe6b92533e3f9d8f6de2b47c18b957561e43
CodingGengar <scriptkiddie231@protonmail.ch>
2020-07-23 04:23:39 -0700

    The code that makes the bot

---
+++ index.js
@@ -0,0 +1,50 @@
+const tmi = require('tmi.js')
+
+const options = {
+  options: {
+    debug: true,
+  },
+  connection: {
+    cluster: 'aws',
+    reconnect: true,
+  },
+  identity: {
+    username: 'SeborBot',
+    password: 'NOPASSWORDHERE',
+  },
+  channels: ['Sebor'],
+};
+
+//Pulls everything over from const tmi
+
+const client = new tmi.client(options);
+
+//Connecting the Bot
+
+client.connect();
+
+//when Bot Connects
+
+client.on('connected', (address, port) => {
+  client.action('Sebor', 'Hi Chat');
+});
+
+//Bot Commands
+
+client.on('chat', function(channel, user, message, self) {
+  if(message === '!links') {
+    client.action('Sebor', ' | Twitter -  https://twitter.com/sebor_  |');
+  }
+  if(message === '!coms') {
+    client.action('Sebor', 'These are the commands for this stream, !links !lurk !bot');
+  }
+  if(message === '!lurk') {
+    client.action('Sebor', 'FeelsWeirdMan');
+  }
+    if(message === '!bot') {
+    client.action('Sebor', 'Im Sebs replacement');
+  }
+});
+
+
+
